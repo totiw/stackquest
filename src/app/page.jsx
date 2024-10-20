41a45f453acc6336b65192166b843008b54dbca8
Toto Jati Wijayanto <97005196+totiw@users.noreply.github.com>
2024-10-20 14:08:37 +0700

    feat: add packages and setup project

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="font-[family-name:var(--font-poppins)]">
+      <main></main>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/main/Navbar.jsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+function Navbar() {
+  return <div>Navbar</div>;
+}
+
+export default Navbar;
